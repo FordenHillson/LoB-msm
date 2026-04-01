f8d2b5b7b3e0317798c0d0239a540489ad787297
F0rD <fordgomfop@gmail.com>
2026-04-01 19:44:41 +0700

    remove strike on anc necro abso

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_luck(good_chance: int):
     is_good = roll <= good_chance
     return roll, is_good
 
-async def send_luck(interaction: discord.Interaction, good_chance: int, bad_chance: int, title: str, show_strikes: int = -1):
+async def send_luck(interaction: discord.Interaction, good_chance: int, bad_chance: int, title: str, show_strikes: int | None = -1):
     roll, is_good = get_luck(good_chance)
     if is_good:
         luck_type = "Good Luck"
@@ -67,21 +67,21 @@ async def luck_anc_command(interaction: discord.Interaction, bonus: int = 0):
     base_good = 30
     good_chance = min(base_good + bonus, 100)
     bad_chance = 100 - good_chance
-    await send_luck(interaction, good_chance=good_chance, bad_chance=bad_chance, title=f"Ancient Craft (Base: {base_good}% + Bonus: {bonus}%)")
+    await send_luck(interaction, good_chance=good_chance, bad_chance=bad_chance, title=f"Ancient Craft (Base: {base_good}% + Bonus: {bonus}%)", show_strikes=None)
 
 @tree.command(name="luck-necro", description="Check your luck on Necromancer (base 4%)")
 async def luck_necro_command(interaction: discord.Interaction, bonus: int = 0):
     base_good = 4
     good_chance = min(base_good + bonus, 100)
     bad_chance = 100 - good_chance
-    await send_luck(interaction, good_chance=good_chance, bad_chance=bad_chance, title=f"Necromancer (Base: {base_good}% + Bonus: {bonus}%)")
+    await send_luck(interaction, good_chance=good_chance, bad_chance=bad_chance, title=f"Necromancer (Base: {base_good}% + Bonus: {bonus}%)", show_strikes=None)
 
 @tree.command(name="luck-abso", description="Check your luck on Absolab (base 12%)")
 async def luck_abso_command(interaction: discord.Interaction, bonus: int = 0):
     base_good = 12
     good_chance = min(base_good + bonus, 100)
     bad_chance = 100 - good_chance
-    await send_luck(interaction, good_chance=good_chance, bad_chance=bad_chance, title=f"Absolab (Base: {base_good}% + Bonus: {bonus}%)")
+    await send_luck(interaction, good_chance=good_chance, bad_chance=bad_chance, title=f"Absolab (Base: {base_good}% + Bonus: {bonus}%)", show_strikes=None)
 
 @tree.command(name="luck-exalt", description="Check your luck on Exaltation (base 50%)")
 async def luck_exalt_command(interaction: discord.Interaction, bonus: int = 0):
